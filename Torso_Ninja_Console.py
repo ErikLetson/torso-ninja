2dba20735e38b076a7beede2382ca8ff3f244d2f
= <=>
2017-07-25 09:15:39 -0500

    Applied license notices where appropriate

---
+++ Torso_Ninja_Console.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env-python
+
+# Torso Ninja - scrolling shooter video game
+# Copyright (C) 2015-2017 Erik Letson <hmagellan@tutamail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+from lib import main
+
+# Print GPL notification to screen
+print ""
+print " Torso Ninja - scrolling shooter video game"
+print " Copyright (C) 2015-2017 Erik Letson <hmagellan (at) tutamail (dot) com>"
+print ""
+print " This program is free software: you can redistribute it and/or modify"
+print " it under the terms of the GNU General Public License as published by"
+print " the Free Software Foundation, either version 3 of the License, or"
+print " (at your option) any later version."
+print ""
+print " This program is distributed in the hope that it will be useful,"
+print " but WITHOUT ANY WARRANTY; without even the implied warranty of"
+print " MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
+print " GNU General Public License for more details."
+print ""
+print " You should have received a copy of the GNU General Public License"
+print " along with this program.  If not, see <http://www.gnu.org/licenses/>."
+print ""
+
+main.Main()
